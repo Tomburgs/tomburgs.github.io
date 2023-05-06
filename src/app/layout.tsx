e73c6ec2a47520fc81e0f6901e95632ad794fa5a
Toms Burgmanis <toms.burgmanis@gmail.com>
2023-05-06 20:36:06 +0200

    fix: add viewport meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { ReactNode } from 'react';
 import './globals.css'
 
@@ -13,6 +14,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="viewport-fit=cover, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
       <body>{children}</body>
     </html>
   )
